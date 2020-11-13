cd604152b5b64d45ffe0156ca0d14f51751621bc
Anatoliy Kravchenko <ankravchen@e1r3p14.unit.ua>
2020-11-13 22:16:20 +0200

    finish t05

--- JS4/t05/js/script.js
+++ JS4/t05/js/script.js
@@ -1,21 +1,21 @@
 let salats = new Map([
-    ['Greek Salad' , 5.99],
-    ['Caesar Salad', 7.99]]
+    ['Greek Salad' , "5.99"],
+    ['Caesar Salad', "7.99"]]
 );
 let main = new Map([
-    ['Margherita Pizza', 12.50],
-    ['Tomato Soup', 6.99],
-    ['Burger', 10.00]
+    ['Margherita Pizza', "12.50"],
+    ['Tomato Soup', "6.99"],
+    ['Burger', "10.00"]
 ]);
 let deserts = new Map([
-    ['Margherita Pizza', 12.50],
-    ['Tomato Soup', 6.99],
-    ['Burger', 10.00]
+    ['Cheesecake', "4.99"],
+    ['Chocolate Ice-Cream', "2.50"],
+    ['Fruit Salad', "3.99"]
 ]);
 let drinks = new Map([
-    ['Lemonade', 3.20],
-    ['Green Tea', 1.50],
-    ['Coffee', 1.99]
+    ['Lemonade', "3.20"],
+    ['Green Tea', "1.50"],
+    ['Coffee', "1.99"]
 ]);
 let menu = new Map([
     ['Salads', salats],
@@ -24,22 +24,19 @@ let menu = new Map([
     ['Drinks', drinks]
     ]);
 let mainContain = document.getElementById('mainContain');
-function renderMenu(obj){
-
-}
-
-let result = menu.keys();
-for (let item of result){
-    console.log(item);
-}
-let value = menu.values();
-let done;
-for(let item of value){
-    let a = Array.from(item);
-    console.log(a[0][1])
-    done = Array.from(item).reduce(function(sum, current){
-      return  sum + `<li><span>${current[0]}</span><span>$ ${current[1]}</span></li>`, `<li><span>${current[0]}</span><span>$ ${current[1]}</span></li>`;
+function renderMenu(){
+    let arr1 = [];
+    let value = menu.values();
+    for(let item of value){
+        arr1.push(Array.from(item).map(current =>`<li><span>${current[0]}</span><span>$ ${current[1]}</span></li>`).join(' '));
+    }
+    let menuTitle = menu.keys();
+    Array.from(menuTitle).forEach((item, index) => {
+        let ul = document.createElement('ul');
+        ul.innerHTML = `<li>${item}</li> ${arr1[index]}`
+        mainContain.append(ul);
     })
-
-    console.log(done);
 }
+renderMenu();
+
+
