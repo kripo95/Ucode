bb5d092877b6af2513cfb85b4f247abe53b61891
Anatoliy Kravchenko <ankravchen@e1r3p14.unit.ua>
2020-11-15 21:04:12 +0200

    t07 start

--- JS4/t06/js/script.js
+++ JS4/t06/js/script.js
@@ -1,9 +1,33 @@
-function* hiGenerator(word){
-    let textInner = word.toLowerCase();
+function* generateSequence(start, end) {
+    for (let i = start; i <= end; i++) yield i;
+}
+let gen = generateSequence(0, 4);
 
-    if(textInner === 'Hi' || textInner === 'Hello' || textInner === 'Darov' || textInner === 'privet'){
-        for (let i = 0; i < 5; i++){
-            yield i;
-        }
+function answerJarvis(word){
+    const phrases = ['Hello, I am J.A.R.V.I.S.', 'I believe I\'ve already said it, but, sure, hello again!', 'You are malfunctioning.', 'I believe your intentions to be hostile.', 'I will not respond to that.'];
+    if(word === 'hi' || word === 'hello' || word === 'privet' || word === 'zdorov' || word === 'hey'){
+        const numberPrase = phrases[gen.next().value];
+        return numberPrase === undefined ? 'Error': numberPrase;
+    } else {
+         return `I don't understand`;
     }
 }
+const sendMessage = document.getElementById('sendMessage');
+const chat = document.getElementById('chat');
+sendMessage.addEventListener('click', function (){
+    const textField = document.getElementById('textField');
+    let inputText = textField.value;
+    if(textField.value){
+        let div = document.createElement('div');
+        div.classList.add('messageContainMe');
+        div.innerHTML = `<span class="messageMe">${inputText}</span>`
+        chat.prepend(div);
+        setTimeout(function (){
+            let div = document.createElement('div');
+            div.classList.add('messageContainJar');
+            div.innerHTML = `<span class="messageJar">${answerJarvis(inputText)}</span>`
+            chat.prepend(div)
+        }, 500);
+    }
+    textField.value = '';
+})
