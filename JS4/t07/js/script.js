81829a322dd9631c5a328f5d70a1f9d5fde20634
Anatoliy Kravchenko <ankravchen@e1r4p16.unit.ua>
2020-11-16 19:17:55 +0200

    t07

--- JS4/t07/js/script.js
+++ JS4/t07/js/script.js
@@ -3,7 +3,38 @@ let user = {
     age: document.getElementById('age').value,
     email: document.getElementById('email').value
 };
+// Don't edit above this line
 
+// user = new Proxy(user, {
+//     set(target, p, value) {
+//         switch (p){
+//             case 'name':
+//                 break;
+//             case 'age':
+//                 if(typeof +value == 'number' && !value.startsWith('0')){
+//                     target[p] = value;
+//                     return true;
+//                 }
+//         }
+//     }
+// });
+function number (item){
+    if(/^0|(\D)/gmi.test(item)){
+        return item;
+    }else {
+        return false;
+    }
+}
+function string(item){
+    if(){
+
+    }else {
+
+    }
+}
+
+
+// Don't edit above this line
 function edit(btn) {
     btn.innerHTML = 'save';
     btn.setAttribute('onclick', 'save(this)');
