d0ccdd9c790742d286b48812e0f4ad13bc9151d7
Anatoliy Kravchenko <ankravchen@e1r3p14.unit.ua>
2020-12-04 20:54:57 +0200

    100%

--- JS4/t13/to-do-list/src/App.js
+++ JS4/t13/to-do-list/src/App.js
@@ -8,18 +8,26 @@ class App extends React.Component {
         super(props);
         this.state = {
             items : [],
-            text : ''
+            text : '',
+            isStart : false
         }
         this.handleOnChange = this.handleOnChange.bind(this);
         this.handlClick = this.handlClick.bind(this);
 
     }
     render() {
+        const  isStart = this.state.isStart;
+        let start;
+        if (isStart) start = <ToDoItems items={this.state.items}/>;
         return (
-            <div>
-                <input type="text" id='toDoValue' onChange={this.handleOnChange} value={this.state.text}/>
-                <input type="submit" onClick={this.handlClick}/>
-                <ToDoItem items={this.state.items}/>
+            <div className="wrapperToDo">
+                <h1>Batman`s to-Do List</h1>
+                <div className="inputWrapper">
+                    <input className="textInput" placeholder='add new item' type="text" id='toDoValue' onChange={this.handleOnChange} value={this.state.text}/>
+                    <input className="submitInput" value='+' type="submit" onClick={this.handlClick}/>
+                </div>
+                {start}
+                <div className='fakeDiv'></div>
             </div>
             )
 
@@ -31,27 +39,53 @@ class App extends React.Component {
         if (this.state.text.length === 0) {
             return;
         }
-        const newItem = this.state.text;
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
 
         this.setState( state => ({
             items: state.items.concat(newItem),
-            text: ''
+            text: '',
+            isStart: true
             })
         );
     }
 }
 
-class ToDoItem extends React.Component {
+class ToDoItems extends React.Component {
     render() {
         return (
-            <div>
-                {this.props.items.map(item => (
-                    <div>{item.text}</div>
+            <div className="taskItemBlock">
+                {this.props.items.map((item, i) => (
+                    <ItemTask key={i} itemId ={item}/>
                 ))}
             </div>
 
         )
     }
 }
+class ItemTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFulfilled : true,
+        };
+        this.handlClickCheck = this.handlClickCheck.bind(this);
+    }
+    handlClickCheck(e){
+        e.target.disabled = 'disabled'
+        this.setState({isFulfilled : false});
+    }
+    render() {
+        return (
+            <div key={this.props.itemId.id} className='itemCheckWrapper' >
+                <label htmlFor={this.props.itemId.text}  className = {this.state.isFulfilled ? "unfulfilledTask" : "fulfilledTask"}>{this.props.itemId.text}</label>
+                <input id={this.props.itemId.text} type='checkbox' onClick={this.handlClickCheck} />
+            </div>
+        )
+    }
+}
 
 export default App;
+
